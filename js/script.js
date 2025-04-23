921d2d264b4547bb784cdd709ce5f0385cbbb05c
Bekmurod <bekmruodgofurov0000@gmail.com>
2025-04-23 15:38:08 +0500

    finish modul 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 window.addEventListener("DOMContentLoaded", () => {
-    let now = new Date()
-  console.log(now.getTime());
-    
   // Tab functions
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabContents = document.querySelectorAll(".tab_content"),
@@ -57,10 +54,10 @@ window.addEventListener("DOMContentLoaded", () => {
     const time = Date.parse(endTime) - Date.parse(new Date());
 
     if (time <= 0) {
-        days=0
-        hours=0
-        minuts=0
-        seconds=0
+      days = 0;
+      hours = 0;
+      minuts = 0;
+      seconds = 0;
     } else {
       days = Math.floor(time / (1000 * 60 * 60 * 24));
       hours = Math.floor((time / (1000 * 60 * 60)) % 24);
@@ -106,6 +103,170 @@ window.addEventListener("DOMContentLoaded", () => {
       }
     }
   }
-
   setClock(".timer", deadline);
+
+  //   Modal
+
+  const openMadalBtns = document.querySelectorAll("[data-modal]");
+  const modal = document.querySelector(".modal");
+  const modalClose = document.querySelector("[data-modal-close]");
+
+  function openModal() {
+    modal.classList.add("show", "fade");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearTimeout(ramainContact);
+  }
+
+  function closeModal() {
+    modal.classList.add("hide");
+    document.body.style.overflow = "";
+    modal.classList.remove("show");
+  }
+
+  openMadalBtns.forEach((btn) => btn.addEventListener("click", openModal));
+
+  modalClose.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if ((event.code = "Escape" && modal.classList.contains("show"))) {
+      closeModal();
+    }
+  });
+
+  const ramainContact = setTimeout(openModal, 10000);
+
+  //Offers Menu
+
+  class OffersMenu {
+    constructor(src, alt, title, descr, discount, sale) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.desct = descr;
+      this.discount = discount;
+      this.sale = sale;
+      this.farmatToUsd();
+      this.render();
+    }
+
+    farmatToUsd() {
+      this.discount = this.discount.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+
+      this.sale = this.sale.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    }
+
+    render() {
+      const motherDiv = document.querySelector(".offers-items");
+      const element = document.createElement("div");
+      element.innerHTML = `
+        <img src="${this.src}" alt="${this.alt}" />
+        <div>
+          <h3>${this.title}</h3>
+          <p>
+            ${this.desct}
+          </p>
+            <p>
+            <del>${this.discount}</del> <span class="primary-text">${this.sale}</span>
+          </p>
+        </div>`;
+      motherDiv.appendChild(element);
+    }
+  }
+
+  const offerArr = [
+    {
+      src: "./img/offer1.png",
+      alt: "Quattro Pasta",
+      title: "Quattro Pasta",
+      dect: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam",
+      discount: 55,
+      sale: 18,
+    },
+    {
+      src: "./img/offer2.png",
+      alt: "Vegertarian Pasta",
+      title: "Vegertarian Pasta",
+      dect: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam",
+      discount: 55,
+      sale: 20,
+    },
+    {
+      src: "./img/offer3.png",
+      alt: "Gluten-Free Pasta",
+      title: "Gluten-Free Pasta",
+      dect: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam",
+      discount: 45,
+      sale: 16,
+    },
+  ];
+
+  offerArr.forEach(item => {
+    new OffersMenu(item.src, item.alt, item.title, item.dect, item.discount, item.sale)
+  })
+
+  class Daytime {
+    constructor(scr, alt, title, pargraph) {
+      this.scr = scr;
+      this.alt = alt;
+      this.title = title;
+      this.pargraph = pargraph;
+      this.render()
+    }
+
+    render() {
+      const daytimeItems = document.querySelector(".daytime-items");
+      const element = document.createElement("div");
+      element.classList.add("daytime-item");
+      element.innerHTML = `
+        <img src="${this.scr}" alt="${this.alt}" />
+        <h3>${this.title}</h3>
+        <p>${this.pargraph}</p>`;
+      daytimeItems.appendChild(element)
+    }
+  }
+
+  const daytimeArr = [
+    {
+      scr: "./img/breckfastIcon.png",
+      alt: "Breakfast",
+      title: "Breakfast",
+      par: "8:00 am to 10:00 am",
+    },
+    {
+      scr: "./img/lunchIcon.png",
+      alt: "Lunch",
+      title: "Lunch",
+      par: "04:00 am to 10:00 am",
+    },
+    {
+      scr: "/img/dinnerIcon.png",
+      alt: "Dinner",
+      title: "Dinner",
+      par: "9:00 pm to 1:00 Am",
+    },
+    {
+      scr: "./img/dessertIcon.png",
+      alt: "Dessert",
+      title: "Dessert",
+      par: "All day",
+    },
+  ];
+
+  daytimeArr.forEach(item => {
+    const {scr, alt, title, par} = item
+    new Daytime(scr, alt, title, par)
+  })
 });
